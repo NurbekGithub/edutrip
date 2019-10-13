5d4734c65be6cd4f549c257f58e24639708088b9
Nurbek <nurbek-2395@mail.ru>
2019-10-13 20:09:28 +0600

    update links

--- src/components/rightSider.js
+++ src/components/rightSider.js
@@ -5,13 +5,22 @@ export default function RightSider() {
   return (
     <aside className="fixed right-0 h-screen flex items-center justify-center w-12 top-0">
       <div className="flex flex-col items-center">
-        <a href="#" className="py-2 text-gray-500 hover:text-teal-400">
+        <a
+          href="https://www.facebook.com/EduTravel-1958334667515174/"
+          className="py-2 text-gray-500 hover:text-teal-400"
+        >
           <FaFacebookF />
         </a>
-        <a href="#" className="py-2 text-gray-500 hover:text-teal-400">
+        <a
+          href="https://instagram.com/edutravel_kz?igshid=1q5jmb15pc0jm"
+          className="py-2 text-gray-500 hover:text-teal-400"
+        >
           <FaInstagram />
         </a>
-        <a href="#" className="py-2 text-gray-500 hover:text-teal-400">
+        <a
+          href="https://vk.com/edutravelkz"
+          className="py-2 text-gray-500 hover:text-teal-400"
+        >
           <FaVk />
         </a>
       </div>
