54eec62063ab6fd8d4a58b8deff6772286f7fd20
Nurbek <nurbek-2395@mail.ru>
2019-11-02 16:25:23 +0600

    map-icons

--- src/components/aboutPage/About.js
+++ src/components/aboutPage/About.js
@@ -3,6 +3,12 @@ import AnimatedCircle from "../AnimatedCircle"
 import { useInView } from "react-intersection-observer"
 import { TabletWidth } from "../../utils/globalStyleObjects"
 import map2 from "../../images/map2.svg"
+import mapIcon1 from "../../images/map-icon.svg"
+import mapIcon2 from "../../images/map-icon2.svg"
+import mapIcon3 from "../../images/map-icon3.svg"
+import mapIcon4 from "../../images/map-icon4.svg"
+import mapIcon5 from "../../images/map-icon5.svg"
+import mapIcon6 from "../../images/map-icon6.svg"
 import { useMediaPredicate } from "react-media-hook"
 
 function About({ about }) {
@@ -16,7 +22,39 @@ function About({ about }) {
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-2">О НАС</h2>
         <div className="flex flex-col md:flex-row">
-          <img src={map2} alt="map" className="md:w-2/5 w-full" />
+          <div className="relative md:w-2/5 w-full">
+            <img src={map2} alt="map" className="w-full" />
+            <img
+              src={mapIcon1}
+              alt="map-icon"
+              className="map-icon map-icon-1"
+            />
+            <img
+              src={mapIcon2}
+              alt="map-icon"
+              className="map-icon map-icon-2"
+            />
+            <img
+              src={mapIcon3}
+              alt="map-icon"
+              className="map-icon map-icon-3"
+            />
+            <img
+              src={mapIcon4}
+              alt="map-icon"
+              className="map-icon map-icon-4"
+            />
+            <img
+              src={mapIcon5}
+              alt="map-icon"
+              className="map-icon map-icon-5"
+            />
+            <img
+              src={mapIcon6}
+              alt="map-icon"
+              className="map-icon map-icon-6"
+            />
+          </div>
           {inView && (
             <div className="md:w-3/5 w-full flex flex-col justify-center">
               <div className="flex mt-4">
--- src/components/homePage/About.js
+++ src/components/homePage/About.js
@@ -4,6 +4,12 @@ import AnimatedCircle from "../AnimatedCircle"
 import { useInView } from "react-intersection-observer"
 import { TabletWidth } from "../../utils/globalStyleObjects"
 import map2 from "../../images/map2.svg"
+import mapIcon1 from "../../images/map-icon.svg"
+import mapIcon2 from "../../images/map-icon2.svg"
+import mapIcon3 from "../../images/map-icon3.svg"
+import mapIcon4 from "../../images/map-icon4.svg"
+import mapIcon5 from "../../images/map-icon5.svg"
+import mapIcon6 from "../../images/map-icon6.svg"
 import { useMediaPredicate } from "react-media-hook"
 
 function CountAndText({ count, text }) {
@@ -11,6 +17,7 @@ function CountAndText({ count, text }) {
   return (
     <p className="mb-4 md:font-bold text-sm md:text-base lg:text-xl flex justify-between sm:block">
       <span className="font-extrabold text-teal-400 sm:text-2xl lg:text-4xl sm:w-1/3 text-right sm:pr-8 inline-block">
+        {count[0] === "+" && "+"}
         <Countup params={{ end: count }} />
       </span>
       <span>{text}</span>
@@ -29,10 +36,42 @@ function About({ about }) {
     <section className="w-full bg-white px-8 py-12 overflow-auto" ref={ref}>
       <div className="container mx-auto">
         <h2 className="text-3xl font-bold mb-2">О НАС</h2>
-        <div className="flex">
-          <img src={map2} alt="map" className="hidden md:block w-2/5" />
+        <div className="flex flex-col lg:flex-row">
+          <div className="relative lg:w-2/5 w-full">
+            <img src={map2} alt="map" className="w-full" />
+            <img
+              src={mapIcon1}
+              alt="map-icon"
+              className="map-icon map-icon-1"
+            />
+            <img
+              src={mapIcon2}
+              alt="map-icon"
+              className="map-icon map-icon-2"
+            />
+            <img
+              src={mapIcon3}
+              alt="map-icon"
+              className="map-icon map-icon-3"
+            />
+            <img
+              src={mapIcon4}
+              alt="map-icon"
+              className="map-icon map-icon-4"
+            />
+            <img
+              src={mapIcon5}
+              alt="map-icon"
+              className="map-icon map-icon-5"
+            />
+            <img
+              src={mapIcon6}
+              alt="map-icon"
+              className="map-icon map-icon-6"
+            />
+          </div>
           {inView && (
-            <div className="w-full md:w-3/5 flex flex-col justify-center">
+            <div className="w-full lg:w-3/5 flex flex-col justify-center">
               <CountAndText count={about.years} text="ГОДА УСПЕШНОЙ РАБОТЫ" />
               <CountAndText
                 count={about.family}
